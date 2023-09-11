6437c75cba802ce1b224a0628ed98b8ca54878dc
Mehmet <sumer682000@gmail.com>
2023-09-11 23:21:35 +0300

    jwt auth implemented

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module linkshortener
 
 go 1.20
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/golang-jwt/jwt/v5 v5.0.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "sync"
-    "crypto/md5"
-    "io"
-    "encoding/hex"
-    "encoding/json"
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"io"
+    "strings"
+	"net/http"
+	"sync"
+    "github.com/golang-jwt/jwt/v5"
 )
 
 var urlMap = make(map[string]string)
@@ -17,17 +19,41 @@ type URLMsg struct {
     URL string `json:"url"`
 }
 
+type User struct {
+    ID int `json:"id"`
+    Username string `json:"username"`
+}
+
 func main() {
     fmt.Println("hello world!")
 
     http.HandleFunc("/", goRedirect) 
-    http.HandleFunc("/shorten", goShorten)
+    http.HandleFunc("/shorten", requireAuth(goShorten))
+    http.HandleFunc("/login", goLogin)
     http.ListenAndServe(":8000", nil)
 
 }
 
+func goLogin(w http.ResponseWriter, r *http.Request) {
+   var user User
+   err := json.NewDecoder(r.Body).Decode(&user)
+   if err != nil {
+       http.Error(w, "Parsing error", http.StatusBadRequest)
+       return
+   }
+   fmt.Println(user)
+   token, err := generateToken(user.ID, user.Username)
+   if err != nil {
+       fmt.Println(err)
+       http.Error(w, "Auth failed", http.StatusUnauthorized)
+       return
+   }
+   fmt.Println(token)
+
+   w.Header().Set("Authorization", "Bearer " + token)
+}
+
 func goRedirect(rw http.ResponseWriter, req *http.Request) {
-    fmt.Println(req.URL.Path)
     shortURL := req.URL.Path[1:]
     longURL, ok := urlMap[shortURL]
 
@@ -37,16 +63,6 @@ func goRedirect(rw http.ResponseWriter, req *http.Request) {
     }
 
     http.Redirect(rw, req, longURL, http.StatusFound)
-}
-
-func generateShortURL(longURL string) string {
-    mu.Lock()
-    defer mu.Unlock()
-
-    hasher := md5.New()
-    io.WriteString(hasher, longURL)
-    hash := hex.EncodeToString(hasher.Sum(nil))
-    return hash[:6]
 }
 
 
@@ -61,3 +77,66 @@ func goShorten(rw http.ResponseWriter, req *http.Request) {
     fmt.Fprintf(rw, "Shortened URL is http://localhost:8000/%s", shortURL)
 }
 
+
+func generateShortURL(longURL string) string {
+    mu.Lock()
+    defer mu.Unlock()
+
+    hasher := md5.New()
+    io.WriteString(hasher, longURL)
+    hash := hex.EncodeToString(hasher.Sum(nil))
+    return hash[:6]
+
+}
+
+
+func generateToken(userID int, username string) (string, error) {
+    token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+        "userID": userID,
+        "username": username,
+    })
+
+    secretKey := []byte("super-secret-key")
+    tokenString, err := token.SignedString(secretKey)
+    if err != nil {
+        return "", err
+    }
+    return tokenString, nil
+}
+
+func requireAuth(next http.HandlerFunc) http.HandlerFunc {
+    return func(w http.ResponseWriter, r *http.Request) {
+        tokenString := extractTokenFromRequest(r)
+
+        if tokenString == "" {
+            http.Error(w, "Unauthorized", http.StatusUnauthorized)
+            return
+        }
+
+        token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+            return []byte("super-secret-key"), nil
+        })
+
+        if err != nil || !token.Valid {
+            http.Error(w, "Unauthorized", http.StatusUnauthorized)
+            return
+        }
+
+        next.ServeHTTP(w, r)
+    }
+}
+
+func extractTokenFromRequest(r *http.Request) string {
+    authHeader := r.Header.Get("Authorization")
+
+    if authHeader == "" {
+        return ""
+    }
+
+    parts := strings.Split(authHeader, " ")
+    if len(parts) != 2 || parts[0] != "Bearer" {
+        return ""
+    }
+
+    return parts[1]
+}
